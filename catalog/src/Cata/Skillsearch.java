01ab0fb0c02d5008c9c5de753a5028ce4a82ab1a
sarathKumarsnair <43510266+sarathKumarsnair@users.noreply.github.com>
2021-03-30 08:14:04 +0530

    Update Skillsearch.java

--- catalog/src/Cata/Skillsearch.java
+++ catalog/src/Cata/Skillsearch.java
@@ -23,10 +23,10 @@ public class Skillsearch {
 		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
 		driver.get("https://alexa.amazon.com/spa/index.html#cards");
 		WebElement id = driver.findElement(By.id("ap_email"));
-		id.sendKeys("saratnai@amazon.com");
+		id.sendKeys("sarat123@gmail.com");
 		File file=id.getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(file,new File("C:\\Users\\saratnai\\Pictures\\SavedPictures.png"));
-		driver.findElement(By.id("ap_password")).sendKeys("Sarath@123");
+		driver.findElement(By.id("ap_password")).sendKeys("testing@123");
 		driver.findElement(By.xpath("//label[@for='auth-show-password-checkbox']//i[@class='a-icon a-icon-checkbox']")).click();
 		driver.findElement(By.xpath("//div[@class='a-checkbox a-checkbox-fancy a-control-row a-touch-checkbox']//label//i[@class='a-icon a-icon-checkbox']")).click();
 driver.findElement(By.id("ap_email_icon")).click();
@@ -36,7 +36,7 @@ driver.findElement(By.id("ap_password_icon")).click();
 		System.out.println("header-"+ driver.findElement(By.id("a-popover-header-1")).getText());
 		System.out.println("Content-"+ driver.findElement(By.xpath("//div[@id='a-popover-content-1']")).getText());
 		driver.findElement(By.xpath("//button[contains(@class,'a-button-close a-declarative')]")).click();
-		System.out.println(driver.findElement(By.xpath("//span[contains(text(),'© 1996-2020, Amazon.com, Inc. or its affiliates')]")).getText());
+		System.out.println(driver.findElement(By.xpath("//span[contains(text(),'Â© 1996-2020, Amazon.com, Inc. or its affiliates')]")).getText());
 		
 	}
 
